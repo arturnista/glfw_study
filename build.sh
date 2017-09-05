31455436cfeba74ce93d2134e0507af033931394
Artur Nista <arturnista@gmail.com>
2017-09-05 12:49:53 -0300

    First files. FT added

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -std=c++11 -lglfw3 -pthread -lGLEW -lGLU -lGL -lrt -lXrandr -lXxf86vm -lXi -lXinerama -lX11 -ldl
---
+++ install.sh
@@ -0,0 +1,8 @@
+apt-get install libglfw3-dev
+
+echo ""
+echo ""
+echo "To compile use:"
+echo "g++ main.cpp -std=c++11 -lglfw3 -pthread -lGLEW -lGLU -lGL -lrt -lXrandr -lXxf86vm -lXi -lXinerama -lX11 -ldl"
+echo ""
+echo ""
